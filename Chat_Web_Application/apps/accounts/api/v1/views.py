76e5d3b53d15f1f2c971fc683c8e43e0d8c8bd1a
Drashti-Makadiya <drashtimakadiya31@gmail.com>
2026-05-27 18:07:36 +0530

    Create registration API

--- Chat_Web_Application/apps/accounts/api/v1/views.py
+++ Chat_Web_Application/apps/accounts/api/v1/views.py
@@ -1,1 +1,12 @@
 from rest_framework.views import APIView
+from apps.utils.api_response import api_response
+from apps.accounts.models import User
+from apps.accounts.api.v1.serializers import RegisterSerializer
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return api_response(success=True, message="User registered successfully", data={"user_id": user.id})
+        return api_response(success=False, message="Registration failed", errors=serializer.errors)
