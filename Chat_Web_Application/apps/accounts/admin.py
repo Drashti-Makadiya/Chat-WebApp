a6bd1b027626fc8b43d298d5d01e7557d09d2eed
Drashti-Makadiya <drashtimakadiya31@gmail.com>
2026-05-28 12:51:47 +0530

    Changes in models.py file manager code

--- Chat_Web_Application/apps/accounts/admin.py
+++ Chat_Web_Application/apps/accounts/admin.py
@@ -3,7 +3,6 @@ from apps.accounts.models import User
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email', 'is_active', 'is_staff','is_online','phone_number')
+    list_display = ('id', 'username', 'email', 'role', 'is_active', 'is_staff', 'is_online', 'phone_number')
     search_fields = ('username', 'email')
-    list_filter = ('is_active', 'is_staff')
-    
+    list_filter = ('is_active', 'is_staff', 'role')
--- Chat_Web_Application/apps/accounts/models.py
+++ Chat_Web_Application/apps/accounts/models.py
@@ -3,22 +3,84 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 from django.db import models
 
 
+# class UserManager(BaseUserManager):
+#     def create_user(self, email, username, password=None, phone_number=None, **extra_fields):
+#         if not email:
+#             raise ValueError("Email is required")
+
+#         email = self.normalize_email(email)
+#         user = self.model(email=email, username=username, phone_number=phone_number, **extra_fields)
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+
+#     def create_superuser(self, email, username, password=None, phone_number=None, **extra_fields):
+#         user = self.create_user(email, username, password, phone_number, **extra_fields)
+#         user.is_staff = True
+#         user.is_superuser = True
+#         user.save(using=self._db)
+#         return user
 class UserManager(BaseUserManager):
-    def create_user(self, email, username, password=None, phone_number=None, **extra_fields):
+
+    def create_user(
+        self,
+        email,
+        username,
+        password=None,
+        **extra_fields
+    ):
+        """
+        Create and return a regular user.
+        """
+
         if not email:
-            raise ValueError("Email is required")
+            raise ValueError("Email field is required")
+
+        if not username:
+            raise ValueError("Username field is required")
 
         email = self.normalize_email(email)
-        user = self.model(email=email, username=username, phone_number=phone_number, **extra_fields)
+
+        user = self.model(
+            email=email,
+            username=username,
+            **extra_fields
+        )
+
         user.set_password(password)
         user.save(using=self._db)
+
         return user
 
-    def create_superuser(self, email, username, password=None, phone_number=None, **extra_fields):
-        user = self.create_user(email, username, password, phone_number, **extra_fields)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
+    def create_superuser(
+        self,
+        email,
+        username,
+        password=None,
+        **extra_fields
+    ):
+        """
+        Create and return a superuser.
+        """
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("role", "admin")
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+
+        user = self.create_user(
+            email=email,
+            username=username,
+            password=password,
+            **extra_fields
+        )
+
         return user
 
 
@@ -31,6 +93,12 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_online = models.BooleanField(default=False)
     last_seen = models.DateTimeField(null=True, blank=True)
     profile_image = models.ImageField(upload_to="profiles/", null=True, blank=True)
+    
+    class Roles(models.TextChoices):
+        USER = "user", "User"
+        ADMIN = "admin", "Admin"
+    role = models.CharField(max_length=50, default="user", choices=Roles.choices)  # e.g., user, admin, moderator
+    
 
     # Django required fields
     is_active = models.BooleanField(default=True)
