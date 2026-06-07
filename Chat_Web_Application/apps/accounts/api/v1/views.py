1b4df1dbd78a4a5a137f37dbea99277545b1e248
Drashti-Makadiya <drashtimakadiya31@gmail.com>
2026-06-07 15:18:26 +0530

    changes added

--- Chat_Web_Application/apps/accounts/api/v1/views.py
+++ Chat_Web_Application/apps/accounts/api/v1/views.py
@@ -20,7 +20,7 @@ class RegisterView(APIView):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            print("User created:", user)
+            print("User:", user)
             return api_response(success=True, message="User registered successfully", data=None, status_code=201)
         return api_response(success=False, message="Registration failed", errors=serializer.errors, status_code=400)
     
